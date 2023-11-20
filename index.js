160d6f2b6f228203ab3ce4011495b48c072d06c8
truongcongtrung <truongcongtrung2709@gmail.com>
2023-11-20 17:22:40 +0700

    fixed

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
+
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+// Use default router
+server.use(router);
+server.listen(8080, () => {
+  console.log("JSON Server is running");
+});
